7edd01517e84f413173915b79ec804cc33a3f084
J <80164315+jbolns@users.noreply.github.com>
2023-05-21 17:33:31 +0300

    Import typo

--- src/pages/roadmap.js
+++ src/pages/roadmap.js
@@ -1,10 +1,10 @@
 import * as React from "react"
 import Layout from '../components/layout'
 import Seo from '../components/seo'
+import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Card from 'react-bootstrap/Card'
-import { Container } from "react-bootstrap"
 
 const RoadmapPage = () => {
     return (
